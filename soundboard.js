ce38abeb0ebac07a22de70c81f9ba70b08895afc
langerno <langerno@msu.edu>
2022-11-19 18:25:26 -0500

    Added header, mouseenter

--- soundboard.js
+++ soundboard.js
@@ -5,6 +5,15 @@ var virusButton = document.getElementById('virus-sound');
 var vomit = new Audio('out_with_it_x.wav');
 var laugh = new Audio('laugh_cough.wav');
 var virus = new Audio('w32netskyc.wav');
+var enter = new Audio('click_x.wav');
+
+function playclick(){
+    enter.play();
+}
+
+vomitButton.addEventListener('mouseenter', playclick);
+laughButton.addEventListener('mouseenter', playclick);
+virusButton.addEventListener('mouseenter', playclick);
 
 vomitButton.addEventListener('click', function () { vomit.play() });
 laughButton.addEventListener('click', function () { laugh.play() });
